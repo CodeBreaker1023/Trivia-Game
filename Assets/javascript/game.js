c60d1a203826853b21b2288691d17e12b56ef104
ANS NEOphyte <jpbrown1023@gmail.com>
2018-08-26 01:57:03 -0400

    questions added

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,6 +1,63 @@
 // Declare variables
 var clockRunning = false;
 var intervalId;
+var questionArray = [
+    {
+        prompt: "Who is Snow White's Prince?\n(a) Prince Phillip \n\
+                (b) Prince Florian\n(c) Prince Charming",
+        answer: "b"
+    },
+    {
+        prompt: "In the Lion King, what is Simba's mother's name?\n\
+                (a) Sarabi\n(b) Nala\n(c) Marembe",
+        answer: "a"
+    },
+    {
+        prompt: "In Aladin, how long was Genie stuck in the lamp?\n\
+                (a) 1,000 years\n(b) 5,000 years\n(c) 10,000 years",
+        answer: "c"
+    },
+    {
+        prompt: "In the Jungle Book, who teaches Mowgli the Bare Necessities?\n\
+                (a) Maguera\n(b) Baloo\n(c) King Louie",
+        answer: "b"
+    },
+    {
+        prompt: "Cruella de Vil is the villian in which Disney movie?\n\
+                (a) The Rescuers\n(b) 101 Dalmations\n(c) The Aristocats",
+        answer: "b",
+    },
+    {
+        prompt: "In Finding Nemo, what's the name of the turtle that Nemo\n\
+                rides the East Australian Current with? (a) Curl\n(b) Surf\n\
+                (c) Crush",
+        answer: "c"
+    },
+    {
+        prompt: "In the Disney movie Robin Hood, what type of animal was Robin?\n\
+                (a) fox\n(b) bear\n(c) rabbit",
+        answer: "a"
+    },
+    {
+        promt:  "What is the owl's name in The Sword in the Stone?\n\
+                (a) Merlin\n(b) Archimedes\n(c) Bernard",
+        answer: "b"
+    },
+    {
+        prompt: "In the Emperor's New Groove, what animal does Kuzco turn into?\n\
+                (a) donkey\n(b) elephant\n(c) llama",
+        answer: "c"
+    },
+    {
+        prompt: "In what year did Alice in Wonderland debut? (a) 1941\n\
+                (b) 1951\n(c) 1961",
+        prompt: "b"
+    }
+    }
+        
+    }
+    }
+var answerArray = ["a. Prince Charming b. "]
 
 // Create start button
 $("#start").on("click", function() {
@@ -30,6 +87,7 @@ var timer = {
         if (!clockRunning) {
           intervalId = setInterval(timer.count, -1000);
           clockRunning = true;
+          console.log("start function");
         } else if (timer.time === 0){
             alert("Sorry you ran out of time!");
             clockRunning = false;
